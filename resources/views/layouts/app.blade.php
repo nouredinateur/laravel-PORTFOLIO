0eeda1a30480e4e308f757c99a702ea2ae237a06
nnafzaoui <nafzaouinourdine@gmail.com>
2021-08-02 08:05:52 +0100

    done home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,11 @@
 <body>
     <nav class="w-full h-16	shadow flex justify-between">
         <div class="ml-16">
-            <img class="h-16" src="{{ asset('img/logo.png') }}" alt="">
+            <img class="h-16" src="{{ asset('img/logo.png') }}" alt="Logo ilustration of me">
         </div>
         <div class="p-5 mr-16">
             <ul class="flex items-center">
-                <li class=""><a class="p-6 text.orange.orange href="">Home</a></li>
+                <li class=""><a class="p-6 text-orange" href="">Home</a></li>
                 <li><a class="p-6" href="">Blog</a></li>
                 <li><a class="p-6" href="">Contact</a></li>
             </ul>
@@ -26,6 +26,32 @@
 
     @yield('content')
     
+    <footer class="w-full mt-40 bg-black h-52"> 
+        <div class="h-full flex justify-between">
+            <div class="h-full pt-8 pl-8" >
+                <img class="h-4/5"src="{{ asset('img/logo.png') }}" alt="Logo ilustration of me">
+            </div>
+            <div class=" my-24 text-white">
+                <ul class="flex items-center">
+                    <li class=""><a class="p-6"  href="">Home</a></li>
+                    <li><a class="p-6" href="">Blog</a></li>
+                    <li><a class="p-6" href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="text-white pr-8 my-24">
+                <div>
+                    <p>You can find me on these Platforms</p>
+                </div>
+                <div class="ml-8 mt-4 text-4xl">
+                    <a href="https://github.com/nnafzaoui" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="https://twitter.com/Mmusashi20" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="https://codepen.io/nnafzaoui" target="_blank"><i class="fab fa-codepen"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script src="https://kit.fontawesome.com/21917d46d7.js" crossorigin="anonymous"></script>
 </html>
--- resources/views/projects/projects.blade.php
+++ resources/views/projects/projects.blade.php
@@ -1,11 +1,61 @@
 
 <div>
-       <h3>Featured Work</h3>
-
-       <div class=" mx-14 my-14 shadow-lg box-border	w-3/12	rounded-b-lg">
-           <img src="{{ asset('img/project.png') }}" alt="">
-           <div class="h-16 p-4	">
-               <p class="font-semibold	">Landing page</p>
-           </div>
-       </div>
+    <h3 class="font-mermelad font-bold text-3xl ml-14 mt-14">Featured Work</h3>
+    <div class="grid grid-cols-3 gap-x-0 w-full px-16">
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-white rounded-2xl bg-darkblue shadow">
+                    <p>School Project</p>
+                </div>
+            </div>
+        </div>
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-white rounded-2xl bg-darkblue shadow">
+                    <p>School Project</p>
+                </div>
+            </div>
+        </div>
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-white rounded-2xl bg-orange shadow">
+                    <p>Side Project</p>
+                </div>
+            </div>
+        </div>
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-orange rounded-2xl bg-black">
+                    <p>Freelance</p>
+                </div>
+            </div>
+        </div>
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-white rounded-2xl bg-orange">
+                    <p>Side Project</p>
+                </div>
+            </div>
+        </div>
+        <div class=" mx-14 my-14 shadow-lg box-border	w-4/5	rounded-b-lg">
+            <img src="{{ asset('img/project.png') }}" alt="">
+            <div class="h-max p-4	">
+                <p class="font-semibold	">Landing page</p>
+                <div class="w-max p-2 my-2 text-white rounded-2xl bg-darkblue">
+                    <p>School Project</p>
+                </div>
+            </div>
+        </div>
+    </div>
+      
 </div>
